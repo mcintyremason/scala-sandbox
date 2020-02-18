e15fe848687f70b904373a0ad4103660250c252d
Mason McIntyre <mmcintyre@shift4.com>
2020-02-18 18:27:04 -0500

    For, Match, and AdjacentElementsProduct with Option type

--- AdjacentElementsProduct/AdjacentElementsProduct.scala
+++ AdjacentElementsProduct/AdjacentElementsProduct.scala
@@ -1,15 +1,23 @@
 object AdjacentElementsProduct {
   def main(args: Array[String]) = {
     println(adjacentElementsProduct(Array(1, 2, 3, 4, 5)))
+    // println(adjacentElementsProduct(Array(-23, 4, -3, 8, -12)))
   }
 
   def adjacentElementsProduct(inputArray: Array[Int]): Int = {
-    var largestProduct = 0;
+    var largestProduct:Option[Int] = None;
+    var lastElement:Option[Int] = None
 
     inputArray.foreach { x =>
-      println(x)
+      if (largestProduct.isEmpty && !lastElement.isEmpty) {
+        largestProduct = Some(lastElement.get * x)
+      } else if (!lastElement.isEmpty && (x * lastElement.get > largestProduct.get)) {
+        largestProduct = Some(x * lastElement.get)
+      }
+      
+      lastElement = Some(x)
     }
 
-    largestProduct
+    largestProduct.get
   }
 }
