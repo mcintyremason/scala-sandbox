c5597ffc240d135e1d9478b4877599e3141c674a
mcintyremason <mcintyre.mlm@gmail.com>
2020-02-19 22:04:56 -0500

    AlmostIncreasingSequence

--- AlmostIncreasingSequence/AlmostIncreasingSequence.scala
+++ AlmostIncreasingSequence/AlmostIncreasingSequence.scala
@@ -4,23 +4,27 @@ object AlmostIncreasingSequence extends App {
 		var outOfOrderCount = 0
     var previousElement:Option[Int] = None
     var previousElementIndex:Option[Int] = None
+    var previousPreviousElement: Option[Int] = None
     var sequenceBuffer = sequence.toBuffer
     val maxValue = sequenceBuffer.reduce((a, b) => a max b)
+    val minValue = sequenceBuffer.reduce((a, b) => a min b)
 
     if(sequenceBuffer.diff(sequenceBuffer.distinct).length > 1) {
       isValid = false
     } else {
       sequenceBuffer.zipWithIndex.map { case (element, index) => 
         if (!previousElement.isEmpty) {
-          println(s"Element: ${element}, Previous Element: ${previousElement.get}")
-          println(1)
-
           if (element <= previousElement.get) {
             if (outOfOrderCount < 1) {
-               if(previousElement.get == maxValue) {
-                // println(s"Element: ${element}, Previous Element: ${previousElement.get}")
-
-                sequenceBuffer.remove(previousElementIndex.get)
+              if(minValue == element) {
+                sequenceBuffer.remove(index)
+                outOfOrderCount += 1
+              } else if (previousElement.get == maxValue) {
+                if(element <= sequenceBuffer(previousElementIndex.get - 1)) {
+                  isValid = false
+                } else {
+                  sequenceBuffer.remove(previousElementIndex.get)
+                }
                 previousElement = Some(element)
                 previousElementIndex = Some(index)
               } else {
@@ -35,14 +39,11 @@ object AlmostIncreasingSequence extends App {
             previousElementIndex = Some(index)
           }
         } else {
-          if (((sequenceBuffer.length - 1) - sequenceBuffer.indexOf(maxValue)) > 1) {
-            sequenceBuffer.remove(sequenceBuffer.indexOf(maxValue))
-            outOfOrderCount += 1
-          }
-
           if (element != maxValue) {
             previousElement = Some(element)
             previousElementIndex = Some(index)
+          } else {
+            outOfOrderCount += 1
           }
         }
       }
@@ -56,6 +57,7 @@ object AlmostIncreasingSequence extends App {
   // val sequence = Array(1, 2, 1, 2, 2)
   // val sequence = Array(1, 2, 3, 4, 99, 5, 6)
   // val sequence = Array(40, 50, 60, 10, 20, 30)
-  val sequence = Array(10, 1, 2, 3, 4, 5)
+  // val sequence = Array(10, 1, 2, 3, 4, 5)
+  val sequence = Array(3, 5, 67, 98, 3)
 	println(almostIncreasingSequence(sequence))
 }
