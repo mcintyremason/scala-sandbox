948cefccec0a1a1ebf3eb975513cd331d280cc32
mcintyremason <mcintyre.mlm@gmail.com>
2020-02-24 20:47:37 -0500

    MatrixElementsSum (Haunted rooms problem)

--- MatrixElementsSum/MatrixElementsSum.scala
+++ MatrixElementsSum/MatrixElementsSum.scala
@@ -1,27 +1,45 @@
 object MatrixElementsSum extends App {
   def matrixElementsSum(matrix: Array[Array[Int]]): Int = {
-    val free = 0
-    var prevRooms = Array[Int]()
+    val numberOfFloors = matrix(0).length
+    var totalCost = 0
+    var isRoomSafe = Array.fill[Boolean](numberOfFloors)(true)
 
-    matrix.zipWithIndex.map { case (element, index) => 
-      // println(matrix(index)(index))
-      matrix(index).zipWithIndex.map { case (element, index) => 
-        // println(element)
-
-        println(prevRooms(index) == null)
-
-        // if(prevRooms(index))
+    for (floorIndex <- 0 to matrix.length - 1) {
+      for (roomIndex <- 0 to matrix(floorIndex).length - 1) {
+        if (floorIndex > 0) {
+          if (matrix(floorIndex-1)(roomIndex) != 0) {
+            if (matrix(floorIndex)(roomIndex) != 0 && isRoomSafe(roomIndex)) {
+              totalCost += matrix(floorIndex)(roomIndex)
+            } else {
+              isRoomSafe(roomIndex) = false
+            }
+          } else {
+            isRoomSafe(roomIndex) = false
+          }
+        } else {
+          if (matrix(floorIndex)(roomIndex) != 0 && isRoomSafe(roomIndex)) {
+            totalCost += matrix(floorIndex)(roomIndex)
+          } else {
+            isRoomSafe(roomIndex) = false
+          }
+        }
       }
     }
-
-    0
+    
+    totalCost
   }
 
+  // val matrix = Array(
+  //   Array(0, 1, 1, 2),
+  //   Array(0, 5, 0, 0),
+  //   Array(2, 0, 3, 3)
+  // )
+
   val matrix = Array(
-    Array(0, 1, 1, 2),
-    Array(0, 5, 0, 0),
-    Array(2, 0, 3, 3)
+    Array(1, 1, 1, 0),
+    Array(0, 5, 0, 1),
+    Array(2, 1, 3, 10)
   )
   
-  matrixElementsSum(matrix)
+  println(matrixElementsSum(matrix))
 }
