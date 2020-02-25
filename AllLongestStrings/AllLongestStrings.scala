53e03697209ef5029e502e9e18cc6a6dd4abc93d
mcintyremason <mcintyre.mlm@gmail.com>
2020-02-24 23:42:23 -0500

    AllLongestStrings

---
+++ AllLongestStrings/AllLongestStrings.scala
@@ -0,0 +1,30 @@
+import scala.collection.mutable.ListBuffer
+
+object AllLongestStrings extends App {
+  def allLongestStrings(inputArray: Array[String]): Array[String] = {
+    var longestStrings = new ListBuffer[String]()
+    var longestStringLength = 0
+
+    for (string <- inputArray) {
+      if (string.length() > longestStringLength) {
+        if (longestStrings.isEmpty) {
+          longestStringLength = string.length()
+          longestStrings += string
+        } else {
+          longestStrings.remove(0, longestStrings.length - 1)
+          longestStrings.remove(0)
+          longestStringLength = string.length()
+          longestStrings += string
+        }
+      } else if (string.length() == longestStringLength) {
+        longestStrings += string
+      }
+    }
+
+    longestStrings.toArray
+  }
+
+  val inputArray = Array("aba", "aa", "ad", "vcd", "aba")
+
+  println(allLongestStrings(inputArray))
+}
