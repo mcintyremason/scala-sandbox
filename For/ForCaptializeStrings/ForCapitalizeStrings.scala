20ba8da8f8ea21b982e12209ea66df78d77182ac
Mason McIntyre <mmcintyre@shift4.com>
2020-02-26 13:40:30 -0500

    Reorganize folder structure

--- For/ForCaptializeStrings/ForCapitalizeStrings.scala
+++
@@ -1,8 +0,0 @@
-object ForCapitalizeStrings extends App {
-	
-	val names = List("adam", "david", "frank")
-	val ucNames = for (name <- names) yield name.capitalize
-
-	for(name <- ucNames) println(name)
-
-}
--- For/ForDoubleNumbers/ForDoubleNumbers.scala
+++
@@ -1,8 +0,0 @@
-object ForDoubleNumbers extends App {
-
-	val nums = Seq(1,2,3)
-	val doubledNums = for (n <- nums) yield n * 2
-	
-	println(doubledNums)
-
-}
